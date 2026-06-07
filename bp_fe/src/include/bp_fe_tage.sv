05115c9224a510f45941a02ba7794e210b8d76d0
fhj20050726 <fhj20050726@gmail.com>
2026-06-07 16:40:56 -0700

    Add files via upload

---
+++ bp_fe/src/include/bp_fe_tage.sv
@@ -0,0 +1,458 @@
+/*
+ * bp_fe_tage.sv
+ *
+ * Verilator-safe TAGE conditional branch direction predictor.
+ * Drop-in replacement for bp_fe_bht.
+ */
+
+`include "bp_common_defines.svh"
+`include "bp_fe_defines.svh"
+
+module bp_fe_tage
+ import bp_common_pkg::*;
+ import bp_fe_pkg::*;
+ #(parameter bp_params_e bp_params_p = e_bp_default_cfg
+   `declare_bp_proc_params(bp_params_p)
+
+   , localparam base_idx_width_lp = bht_idx_width_p
+   , localparam base_els_lp       = 2**base_idx_width_lp
+
+   , localparam tage_tables_lp    = 4
+   , localparam tage_idx_width_lp = (bht_idx_width_p < 8) ? bht_idx_width_p : 8
+   , localparam tage_els_lp       = 2**tage_idx_width_lp
+   , localparam tage_tag_width_lp = 8
+   , localparam provider_width_lp = (tage_tables_lp <= 1) ? 1 : $clog2(tage_tables_lp)
+   )
+  (input                                   clk_i
+   , input                                 reset_i
+
+   , output logic                          init_done_o
+
+   , input                                 w_v_i
+   , input                                 w_force_i
+   , input [bht_idx_width_p-1:0]           w_idx_i
+   , input [bht_offset_width_p-1:0]        w_offset_i
+   , input [ghist_width_p-1:0]             w_ghist_i
+   , input [bht_row_width_p-1:0]           w_val_i
+   , input                                 w_correct_i
+   , output logic                          w_yumi_o
+
+   , input                                 r_v_i
+   , input [vaddr_width_p-1:0]             r_addr_i
+   , input [ghist_width_p-1:0]             r_ghist_i
+   , output logic [bht_row_width_p-1:0]    r_val_o
+   , output logic                          r_pred_o
+   , output logic [bht_idx_width_p-1:0]    r_idx_o
+   , output logic [bht_offset_width_p-1:0] r_offset_o
+   );
+
+  // ------------------------------------------------------------
+  // Storage
+  // ------------------------------------------------------------
+
+  logic [1:0] base_ctr [base_els_lp-1:0];
+
+  logic                         tage_valid  [tage_tables_lp-1:0][tage_els_lp-1:0];
+  logic [tage_tag_width_lp-1:0] tage_tag    [tage_tables_lp-1:0][tage_els_lp-1:0];
+  logic [2:0]                   tage_ctr    [tage_tables_lp-1:0][tage_els_lp-1:0];
+  logic [1:0]                   tage_useful [tage_tables_lp-1:0][tage_els_lp-1:0];
+
+  // ------------------------------------------------------------
+  // Initialization
+  // ------------------------------------------------------------
+
+  assign init_done_o = ~reset_i;
+
+  integer init_i;
+  integer init_t;
+
+  initial begin
+    for (init_i = 0; init_i < base_els_lp; init_i = init_i + 1)
+      base_ctr[init_i] = 2'b01;
+
+    for (init_t = 0; init_t < tage_tables_lp; init_t = init_t + 1) begin
+      for (init_i = 0; init_i < tage_els_lp; init_i = init_i + 1) begin
+        tage_valid [init_t][init_i] = 1'b0;
+        tage_tag   [init_t][init_i] = '0;
+        tage_ctr   [init_t][init_i] = 3'b011;
+        tage_useful[init_t][init_i] = 2'b00;
+      end
+    end
+  end
+
+  // ------------------------------------------------------------
+  // Utility functions
+  // ------------------------------------------------------------
+
+  function automatic int min_int(input int a, input int b);
+    begin
+      min_int = (a < b) ? a : b;
+    end
+  endfunction
+
+  function automatic int hist_len(input int table_id);
+  begin
+    case (table_id)
+      0: hist_len = min_int(2,  ghist_width_p);
+      1: hist_len = min_int(4,  ghist_width_p);
+      2: hist_len = min_int(6, ghist_width_p);
+      default: hist_len = min_int(8, ghist_width_p);
+    endcase
+  end
+endfunction
+
+  function automatic [tage_idx_width_lp-1:0] fold_hist_idx
+    (input [ghist_width_p-1:0] ghist
+     , input int hlen
+     );
+    integer i;
+    begin
+      fold_hist_idx = '0;
+      for (i = 0; i < ghist_width_p; i = i + 1)
+        if (i < hlen)
+          fold_hist_idx[i % tage_idx_width_lp] =
+            fold_hist_idx[i % tage_idx_width_lp] ^ ghist[i];
+    end
+  endfunction
+
+  function automatic [tage_tag_width_lp-1:0] fold_hist_tag
+    (input [ghist_width_p-1:0] ghist
+     , input int hlen
+     );
+    integer i;
+    begin
+      fold_hist_tag = '0;
+      for (i = 0; i < ghist_width_p; i = i + 1)
+        if (i < hlen)
+          fold_hist_tag[i % tage_tag_width_lp] =
+            fold_hist_tag[i % tage_tag_width_lp] ^ ghist[i];
+    end
+  endfunction
+
+  function automatic [tage_idx_width_lp-1:0] tage_index
+    (input [bht_idx_width_p-1:0] idx
+     , input [ghist_width_p-1:0] ghist
+     , input int table_id
+     );
+    begin
+      tage_index =
+        idx[tage_idx_width_lp-1:0]
+        ^ fold_hist_idx(ghist, hist_len(table_id))
+        ^ tage_idx_width_lp'(table_id * 13);
+    end
+  endfunction
+
+  function automatic [tage_tag_width_lp-1:0] tage_make_tag
+    (input [bht_idx_width_p-1:0] idx
+     , input [ghist_width_p-1:0] ghist
+     , input int table_id
+     );
+    integer i;
+    begin
+      tage_make_tag =
+        fold_hist_tag(ghist, hist_len(table_id))
+        ^ tage_tag_width_lp'(table_id * 29);
+
+      for (i = 0; i < tage_tag_width_lp; i = i + 1)
+        if (i < bht_idx_width_p)
+          tage_make_tag[i] = tage_make_tag[i] ^ idx[i];
+    end
+  endfunction
+
+  function automatic [1:0] sat_update_2
+    (input [1:0] ctr
+     , input taken
+     );
+    begin
+      if (taken)
+        sat_update_2 = (ctr == 2'b11) ? ctr : ctr + 1'b1;
+      else
+        sat_update_2 = (ctr == 2'b00) ? ctr : ctr - 1'b1;
+    end
+  endfunction
+
+  function automatic [2:0] sat_update_3
+    (input [2:0] ctr
+     , input taken
+     );
+    begin
+      if (taken)
+        sat_update_3 = (ctr == 3'b111) ? ctr : ctr + 1'b1;
+      else
+        sat_update_3 = (ctr == 3'b000) ? ctr : ctr - 1'b1;
+    end
+  endfunction
+
+  function automatic logic weak_ctr(input [2:0] ctr);
+    begin
+      weak_ctr = (ctr == 3'b011) | (ctr == 3'b100);
+    end
+  endfunction
+
+  // ------------------------------------------------------------
+  // Read path
+  // ------------------------------------------------------------
+
+  wire [bht_idx_width_p-1:0] r_idx_li =
+    r_addr_i[2+:bht_idx_width_p];
+
+  wire [bht_offset_width_p-1:0] r_offset_li =
+    r_addr_i[2+bht_idx_width_p+:bht_offset_width_p];
+
+  logic pred_n, alt_pred_n;
+  logic provider_found_n, alt_found_n;
+  logic [provider_width_lp-1:0] provider_n;
+  logic [bht_row_width_p-1:0] row_n;
+
+  integer rt;
+  always_comb begin
+    pred_n = base_ctr[r_idx_li][1];
+    alt_pred_n = base_ctr[r_idx_li][1];
+
+    provider_found_n = 1'b0;
+    alt_found_n = 1'b0;
+    provider_n = '0;
+
+    for (rt = tage_tables_lp-1; rt >= 0; rt = rt - 1) begin
+      if (tage_valid[rt][tage_index(r_idx_li, r_ghist_i, rt)]
+          && (tage_tag[rt][tage_index(r_idx_li, r_ghist_i, rt)]
+              == tage_make_tag(r_idx_li, r_ghist_i, rt))) begin
+
+        if (!provider_found_n) begin
+          provider_found_n = 1'b1;
+          provider_n = provider_width_lp'(rt);
+          pred_n = tage_ctr[rt][tage_index(r_idx_li, r_ghist_i, rt)][2];
+        end
+        else if (!alt_found_n) begin
+          alt_found_n = 1'b1;
+          alt_pred_n = tage_ctr[rt][tage_index(r_idx_li, r_ghist_i, rt)][2];
+        end
+      end
+    end
+
+    if (provider_found_n
+        && weak_ctr(tage_ctr[provider_n][tage_index(r_idx_li, r_ghist_i, int'(provider_n))])
+        && (tage_useful[provider_n][tage_index(r_idx_li, r_ghist_i, int'(provider_n))] == 2'b00))
+      pred_n = alt_pred_n;
+
+    row_n = '0;
+    row_n[(r_offset_li << 1) + 1] = pred_n;
+    row_n[(r_offset_li << 1)]     = 1'b1;
+  end
+
+  always_ff @(posedge clk_i) begin
+    if (reset_i) begin
+      r_idx_o    <= '0;
+      r_offset_o <= '0;
+      r_val_o    <= '0;
+      r_pred_o   <= 1'b0;
+    end
+    else if (r_v_i && init_done_o) begin
+      r_idx_o    <= r_idx_li;
+      r_offset_o <= r_offset_li;
+      r_val_o    <= row_n;
+      r_pred_o   <= pred_n;
+    end
+  end
+
+  // ------------------------------------------------------------
+  // Update path
+  // ------------------------------------------------------------
+
+  wire [`BSG_SAFE_CLOG2(bht_row_width_p)-1:0] w_pred_bit =
+    (w_offset_i << 1'b1) + 1'b1;
+
+  wire old_pred_taken = w_val_i[w_pred_bit];
+  wire actual_taken = w_correct_i ? old_pred_taken : ~old_pred_taken;
+
+  logic update_provider_found, update_alt_found;
+  logic [provider_width_lp-1:0] update_provider;
+  logic update_alt_pred;
+
+  integer ut;
+  always_comb begin
+    update_provider_found = 1'b0;
+    update_alt_found = 1'b0;
+    update_provider = '0;
+    update_alt_pred = base_ctr[w_idx_i][1];
+
+    for (ut = tage_tables_lp-1; ut >= 0; ut = ut - 1) begin
+      if (tage_valid[ut][tage_index(w_idx_i, w_ghist_i, ut)]
+          && (tage_tag[ut][tage_index(w_idx_i, w_ghist_i, ut)]
+              == tage_make_tag(w_idx_i, w_ghist_i, ut))) begin
+
+        if (!update_provider_found) begin
+          update_provider_found = 1'b1;
+          update_provider = provider_width_lp'(ut);
+        end
+        else if (!update_alt_found) begin
+          update_alt_found = 1'b1;
+          update_alt_pred = tage_ctr[ut][tage_index(w_idx_i, w_ghist_i, ut)][2];
+        end
+      end
+    end
+  end
+
+  // Allocation candidate
+  logic alloc_v;
+  logic [provider_width_lp-1:0] alloc_table;
+  logic [tage_idx_width_lp-1:0] alloc_idx;
+  integer alloc_scan;
+  integer provider_int_comb;
+
+  always_comb begin
+    alloc_v = 1'b0;
+    alloc_table = '0;
+    alloc_idx = '0;
+    provider_int_comb = update_provider_found ? int'(update_provider) : -1;
+
+    for (alloc_scan = tage_tables_lp-1; alloc_scan >= 0; alloc_scan = alloc_scan - 1) begin
+      if (!alloc_v && (alloc_scan > provider_int_comb)) begin
+        if (!tage_valid[alloc_scan][tage_index(w_idx_i, w_ghist_i, alloc_scan)]
+            || (tage_useful[alloc_scan][tage_index(w_idx_i, w_ghist_i, alloc_scan)] == 2'b00)) begin
+          alloc_v = 1'b1;
+          alloc_table = provider_width_lp'(alloc_scan);
+          alloc_idx = tage_index(w_idx_i, w_ghist_i, alloc_scan);
+        end
+      end
+    end
+  end
+
+  // Single write request per TAGE array. This avoids Verilator NBA crashes.
+  logic base_wr_v;
+  logic [bht_idx_width_p-1:0] base_wr_idx;
+  logic [1:0] base_wr_data;
+
+  logic tage_valid_wr_v;
+  logic [provider_width_lp-1:0] tage_valid_wr_table;
+  logic [tage_idx_width_lp-1:0] tage_valid_wr_idx;
+  logic tage_valid_wr_data;
+
+  logic tage_tag_wr_v;
+  logic [provider_width_lp-1:0] tage_tag_wr_table;
+  logic [tage_idx_width_lp-1:0] tage_tag_wr_idx;
+  logic [tage_tag_width_lp-1:0] tage_tag_wr_data;
+
+  logic tage_ctr_wr_v;
+  logic [provider_width_lp-1:0] tage_ctr_wr_table;
+  logic [tage_idx_width_lp-1:0] tage_ctr_wr_idx;
+  logic [2:0] tage_ctr_wr_data;
+
+  logic tage_useful_wr_v;
+  logic [provider_width_lp-1:0] tage_useful_wr_table;
+  logic [tage_idx_width_lp-1:0] tage_useful_wr_idx;
+  logic [1:0] tage_useful_wr_data;
+
+  logic provider_pred;
+  logic [tage_idx_width_lp-1:0] provider_idx;
+
+  always_comb begin
+    base_wr_v = 1'b0;
+    base_wr_idx = w_idx_i;
+    base_wr_data = '0;
+
+    tage_valid_wr_v = 1'b0;
+    tage_valid_wr_table = '0;
+    tage_valid_wr_idx = '0;
+    tage_valid_wr_data = 1'b0;
+
+    tage_tag_wr_v = 1'b0;
+    tage_tag_wr_table = '0;
+    tage_tag_wr_idx = '0;
+    tage_tag_wr_data = '0;
+
+    tage_ctr_wr_v = 1'b0;
+    tage_ctr_wr_table = '0;
+    tage_ctr_wr_idx = '0;
+    tage_ctr_wr_data = '0;
+
+    tage_useful_wr_v = 1'b0;
+    tage_useful_wr_table = '0;
+    tage_useful_wr_idx = '0;
+    tage_useful_wr_data = '0;
+
+    provider_pred = 1'b0;
+    provider_idx = '0;
+
+    if (init_done_o && w_v_i) begin
+      base_wr_v = 1'b1;
+      base_wr_data = sat_update_2(base_ctr[w_idx_i], actual_taken);
+
+      if (!w_correct_i && alloc_v) begin
+        tage_valid_wr_v = 1'b1;
+        tage_valid_wr_table = alloc_table;
+        tage_valid_wr_idx = alloc_idx;
+        tage_valid_wr_data = 1'b1;
+
+        tage_tag_wr_v = 1'b1;
+        tage_tag_wr_table = alloc_table;
+        tage_tag_wr_idx = alloc_idx;
+        tage_tag_wr_data = tage_make_tag(w_idx_i, w_ghist_i, int'(alloc_table));
+
+        tage_ctr_wr_v = 1'b1;
+        tage_ctr_wr_table = alloc_table;
+        tage_ctr_wr_idx = alloc_idx;
+        tage_ctr_wr_data = actual_taken ? 3'b100 : 3'b011;
+
+        tage_useful_wr_v = 1'b1;
+        tage_useful_wr_table = alloc_table;
+        tage_useful_wr_idx = alloc_idx;
+        tage_useful_wr_data = 2'b00;
+      end
+      else if (update_provider_found) begin
+        provider_idx = tage_index(w_idx_i, w_ghist_i, int'(update_provider));
+        provider_pred = tage_ctr[update_provider][provider_idx][2];
+
+        tage_ctr_wr_v = 1'b1;
+        tage_ctr_wr_table = update_provider;
+        tage_ctr_wr_idx = provider_idx;
+        tage_ctr_wr_data = sat_update_3(tage_ctr[update_provider][provider_idx], actual_taken);
+
+        if (provider_pred != update_alt_pred) begin
+          tage_useful_wr_v = 1'b1;
+          tage_useful_wr_table = update_provider;
+          tage_useful_wr_idx = provider_idx;
+
+          if (provider_pred == actual_taken) begin
+            tage_useful_wr_data =
+              (tage_useful[update_provider][provider_idx] == 2'b11)
+              ? 2'b11
+              : tage_useful[update_provider][provider_idx] + 1'b1;
+          end
+          else begin
+            tage_useful_wr_data =
+              (tage_useful[update_provider][provider_idx] == 2'b00)
+              ? 2'b00
+              : tage_useful[update_provider][provider_idx] - 1'b1;
+          end
+        end
+      end
+    end
+  end
+
+  always_ff @(posedge clk_i) begin
+    if (base_wr_v)
+      base_ctr[base_wr_idx] <= base_wr_data;
+
+    if (tage_valid_wr_v)
+      tage_valid[tage_valid_wr_table][tage_valid_wr_idx] <= tage_valid_wr_data;
+
+    if (tage_tag_wr_v)
+      tage_tag[tage_tag_wr_table][tage_tag_wr_idx] <= tage_tag_wr_data;
+
+    if (tage_ctr_wr_v)
+      tage_ctr[tage_ctr_wr_table][tage_ctr_wr_idx] <= tage_ctr_wr_data;
+
+    if (tage_useful_wr_v)
+      tage_useful[tage_useful_wr_table][tage_useful_wr_idx] <= tage_useful_wr_data;
+  end
+
+  assign w_yumi_o = init_done_o & w_v_i;
+
+`ifndef SYNTHESIS
+initial begin
+  $display("========== USING BP_FE_TAGE ==========");
+end
+`endif
+
+endmodule
